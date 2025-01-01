7b4f5142d462cfaaea92395c6a39a0284e431bef
New Adventures <dev@newadventures.dev>
2025-01-01 14:40:58 +0000

    Adds favicon

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,8 +1,16 @@
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("blog/favicon.ico"),
+        ),
+    ),
     path(f"{settings.ADMIN_PATH}/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
 ]
