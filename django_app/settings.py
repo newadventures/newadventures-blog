bd0758d75169e5a6592513c1362f27c14778fd57
New Adventures <dev@newadventures.dev>
2025-01-01 12:14:45 +0000

    False debug is true!

--- django_app/settings.py
+++ django_app/settings.py
@@ -30,7 +30,7 @@ load_dotenv(find_dotenv(".env"))
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
